47594f136b13ace0ecc91ddc2e32aa5ad09eac21
Virendra Sonkaria <76589374+virendra-jpg@users.noreply.github.com>
2022-12-01 17:03:38 +0530

    Add files via upload

---
+++ pages/components/RideSelector.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import tw from "tailwind-styled-components/";
+import { carList } from "../../data/carList";
+
+function RideSelector({ pickupCoordinates, dropoffCoordinates }) {
+  const [rideDuration, setRideDuration] = useState(0);
+
+  useEffect(() => {
+    fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1Ijoic29ua2FyaWEiLCJhIjoiY2xiNHZtc3hmMDBqazNuazJ6a21qZzU5aSJ9.Ib9QYZvWMdB9Ep7UYeA-Zg`
+    )
+      .then((res) => res.json())
+      // .then(res => res.json())
+      .then(data => {
+        if (data.routes) {
+        
+                        setRideDuration(data.routes[0].duration / 100);    }})
+
+      .catch((err) => console.log(err));
+  }, [pickupCoordinates, dropoffCoordinates]);
+  return (
+    <Wrapper>
+      <Title>Choose a ride, or swipe up for more</Title>
+      <CarList>
+        {carList.map((car, index) => (
+          <Car key={`uber-${index} `}>
+            <CarImage src={car.imgUrl} />
+            <CarDetails>
+              <Service>{car.service}</Service>
+              <Time>5 min away</Time>
+            </CarDetails>
+            <Price>{"$" + (rideDuration * car.multiplier).toFixed(2)}</Price>
+          </Car>
+        ))}
+      </CarList>
+    </Wrapper>
+  );
+}
+const Wrapper = tw.div`overflow-auto border-b-2 dark:bg-gray-900 dark:border-gray-600`;
+const Title = tw.div`text-gray-500 text-center text-xs py-2 border-b dark:text-white dark:border-gray-600`;
+const CarList = tw.div`p-4`;
+const Car = tw.div`flex justify-between items-center py-4`;
+const CarImage = tw.img`h-14 mr-4`;
+const CarDetails = tw.div`flex flex-col flex-1`;
+const Service = tw.div`font-medium`;
+const Time = tw.div`text-xs text-blue-500 dark:text-blue-300`;
+const Price = tw.div`text-sm`;
+export default RideSelector;
